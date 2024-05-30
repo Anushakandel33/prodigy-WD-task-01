9e7b16443643b929c1f917943c09305ca6cc521d
Anushakandel33 <149295112+Anushakandel33@users.noreply.github.com>
2024-05-30 14:26:51 +0545

    prodigy task1 done

---
+++ menu.js
@@ -0,0 +1,7 @@
+let food1s = document.getElementById('food(1)');
+let food2s = document.getElementById('food(2)');
+let foods = document.getElementById('food');
+
+food1s.addEventListener('click',()=>{
+    foods.style.backgroundImage= "url('food1.jpg')"
+})
